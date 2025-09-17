4283e7ce6e4f42283b6c863924521fca50823f81
Maksim Stashkevich <makstashkevich@gmail.com>
2025-09-17 22:55:37 +0300

    Remove initTranslations

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,4 @@
 // src/index.ts
-
-import { AllTranslations, LocaleType, setLocale, setTranslations } from './model';
-
-/**
- * Инициализирует переводы на сервере.
- * Эту функцию следует вызывать в серверных компонентах Next.js (например, в layout.ts).
- * @param locale Текущая локаль.
- * @param translations Все переводы.
- */
-export const initTranslations = (locale: LocaleType, translations: AllTranslations) => {
-  setLocale(locale);
-  setTranslations(translations);
-};
-
 export { useTranslate } from './hooks/useTranslate';
 export { TranslateProvider } from './components/TranslateProvider';
 export { setLocale, getLocale, setTranslations, useTranslateModel } from './model';
