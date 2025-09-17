247991b80ac8adf8f3a90efabb1886751eb8e0f4
Maksim Stashkevich <makstashkevich@gmail.com>
2025-09-17 23:17:44 +0300

    Remove export hooks

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 // src/index.ts
-export { useTranslate } from './hooks/useTranslate';
 export { TranslateProvider } from './components/TranslateProvider';
 export { setLocale, getLocale, setTranslations, translate, translate as t, useTranslateModel } from './model';
 export type { LocaleType, Translations, ITranslateModelState } from './model';
